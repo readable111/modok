733f0a8a8a2fd7aff20d25e3e6a03b65b3ee5915
readable111 <bowent111@gmail.com>
2026-03-29 10:40:36 -0500

    mvp: reading filenames from a directory

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+mod buffers;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,7 +10,12 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            buffers::open_and_read_buffer,
+            buffers::open_directory,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
