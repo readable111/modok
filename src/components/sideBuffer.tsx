bbc9b2a102d96b93e56337a1626725e966a94af5
readable111 <bowent111@gmail.com>
2026-03-29 15:35:29 -0500

    styling for selected buffer

--- src/components/sideBuffer.tsx
+++ src/components/sideBuffer.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { ReactElement, useState } from "react";
+import React from "react"
 import { invoke } from "@tauri-apps/api/core";
 import { open } from '@tauri-apps/plugin-dialog'
 import '../App.css'
@@ -12,10 +12,23 @@ interface File {
 }
 
 export const SideBuffer = (props : {files:File[]}) => {
+  const [openFile, setOpenFile] = useState<File>()
+  const [selectedElement,setSelectedElement] = useState<Element>()
+
+  async function select(event:React.MouseEvent , file:File){
+    const current = event.currentTarget;
+    if (selectedElement) {
+      selectedElement.classList.remove('selected');
+    }
+    // call some tauri function here to read the file for the first x lines
+    current.classList.add('selected');
+    setSelectedElement(current);
+  }
+
   return (
     <div className="file-tree">
       {props.files.map(file =>(
-        <div className="file-entry">{file.name}</div>
+        <div onClick={select} className="file-entry">{file.name}</div>
       ))}
     </div>
   )
