9224300873e6fb77d565feaa1b0ad831fa3cc419
readable111 <bowent111@gmail.com>
2026-03-30 20:52:09 -0500

    wip: adding the ability to read from a buffer to the frontend

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import { open } from '@tauri-apps/plugin-dialog'
 import { SideBuffer } from "./components/sideBuffer";
+import type { File } from "./types/file.ts"
 import "./App.css";
 
 function App() {
-  const [files, setFiles] = useState<[{}]>([{}])
+  const [files, setFiles] = useState<File[]>([])
+  const [openFile, setOpenFile] = useState<File>()
+  const [fileContents, setFileContents]= useState<String>()
 
   async function pickFolder() {
     const path = await open({
@@ -15,18 +18,26 @@ function App() {
     })
 
     if (path) {
-      const result = await invoke<[{}]>('open_directory', { dirPath: path });
+      const result = await invoke<File[]>('open_directory', { dirPath: path });
       setFiles(result)
       console.log(result)
     }
   }
 
+  async function readFile(e:React.MouseEvent<HTMLDivElement>, file:File) {
+    const contents = await invoke<String>("open_and_read_buffer", {file_path: file.path});
+    setFileContents(contents)
+    e.currentTarget.classList.add("active")
+    setOpenFile(file)
+  }
+
   return (
     <main>
-      <SideBuffer files={files}/>
+      <SideBuffer files={files} readFile={readFile}/>
       <div>
         <button onClick={pickFolder}>Open Folder</button>
       </div>
+      <div>{fileContents}</div>
     </main>
   );
 }
--- src/components/sideBuffer.tsx
+++ src/components/sideBuffer.tsx
@@ -1,21 +1,20 @@
-import { useState } from "react";
+import { SetStateAction, useState, Dispatch } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import { open } from '@tauri-apps/plugin-dialog'
+import type { File } from "../types/file.d.ts"
 import '../App.css'
 
-interface File {
-  name: string,
-  path: string,
-  is_dir: boolean,
-  extension: string
+interface SideBufferProps {
+  files: File[],
+  readFile: (e:React.MouseEvent<HTMLDivElement>, file:File) => Promise<void>
 }
 
-export const SideBuffer = (props : {files:File[]}) => {
+export const SideBuffer = (props :SideBufferProps) => {
   return (
     <div className="file-tree">
       {props.files.map(file =>(
-        <div className="file-entry">{file.name}</div>
+        <div className="file-entry" onClick={(e) => props.readFile(e,file)}>{file.name}</div>
       ))}
     </div>
   )
