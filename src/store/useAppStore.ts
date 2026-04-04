d0926ed0066d03c9c42963fddef3a3af1d9d6209
Tyler Bowen <126030812+readable111@users.noreply.github.com>
2026-04-04 09:34:14 -0500

    feat: styling and reading lines instead of string (#3)
    * chore: read line by line and preserve whitespace
    
    * styling

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -4,16 +4,16 @@ import { File } from "../types/file.ts"
 import { ReactNode } from "react";
 
 interface AppStore {
-  fileBuffer: String,
+  fileBuffer: String[],
   fetchBuffer: (dir: String) => Promise<void>,
 }
 
 export const useAppStore = create<AppStore>((set, get) => ({
-  fileBuffer: "",
+  fileBuffer: [],
 
   fetchBuffer: async (dir) =>{
     try{
-      const files = await invoke<string>("open_and_read_buffer", {filePath: dir})
+      const files = await invoke<string[]>("open_and_read_buffer", {filePath: dir})
       set({fileBuffer: files})
     } catch (err) {
       console.error("Something went wrong", err)
