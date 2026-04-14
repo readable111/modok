de71ee8746b9d1e6c23da00603f908095cc20cdc
readable111 <bowent111@gmail.com>
2026-04-14 06:36:28 -0500

    change the file reading to use the piece table

--- src-tauri/src/buffers.rs
+++ src-tauri/src/buffers.rs
@@ -3,28 +3,22 @@ use std::io::BufReader;
 use std::fs;
 use std::string::FromUtf8Error;
 use std::path::PathBuf;
+use super::piece_table::PieceTree;
 use super::file::File;
 
 
 
 #[tauri::command]
-pub fn open_and_read_buffer(file_path: &str) -> Result<Vec<String>, String> {
+pub fn open_and_read_buffer(file_path: &str) -> Result<String, String> {
     let file = fs::File::open(file_path).map_err(|e| e.to_string())?;
     let mut reader = BufReader::new(file);
     let mut buffer: Vec<u8> = Vec::new();
     reader.read_to_end(&mut buffer).map_err(|e| e.to_string())?;
-    let mut lines: Vec<String> = Vec::new();
-    let mut line_to_add: Vec<u8> = Vec::new();
-    for ch in buffer.iter() {
-        line_to_add.push(*ch);
-        if *ch == b'\n' || *ch == b'\r' {
-            let mut line = String::from_utf8(line_to_add.clone()).map_err(|e| e.to_string())?;
-            lines.push(line);
-            line_to_add.clear();
-        }
-    }
+    
+    let mut s: &str = std::str::from_utf8(&buffer).expect("invalid UTF 8");
+    let mut piece_tree = PieceTree::new(s);
 
-    Ok(lines)
+    Ok(piece_tree.get_text())
 }
 
 #[tauri::command]
