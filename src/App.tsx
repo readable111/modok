2f6628cca6f170e54e4284815f6c0d3457aa7140
Tyler Bowen <126030812+readable111@users.noreply.github.com>
2026-06-21 15:19:51 -0500

    chore: add piece tree to state (#6)
    * wip: adding the ability to read from a buffer to the frontend
    
    * chore: add piece tree to managed tauri state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import { open } from '@tauri-apps/plugin-dialog'
 import { SideBuffer } from "./components/sideBuffer";
 import { useAppStore } from "./store/useAppStore";
+import { File } from "./types/file";
 import "./App.css";
 
 function App() {
@@ -18,10 +18,15 @@ function App() {
     })
 
     if (path) {
-      const result = await invoke<[{}]>('open_directory', { dirPath: path });
+      const result = await invoke<File[]>('open_directory', { dirPath: path });
       setFiles(result)
       console.log(result)
     }
+  }
+
+  async function readFile(e: React.MouseEvent<HTMLDivElement>, file: File) {
+    await fetchBuffer(file.path)
+    e.currentTarget.classList.add("active")
   }
 
   return (
---
+++ src/types/file.d.ts
@@ -0,0 +1,6 @@
+export interface File {
+  name: string,
+  path: string,
+  is_dir: boolean,
+  extension: string
+}
